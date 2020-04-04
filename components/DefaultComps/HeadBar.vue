d362e935556ff8bba4063844c73dd59b231c0100
tboyd4 <tboyd1898@gmail.com>
2020-04-04 09:40:39 -0500

    headbar styles and content, added new filler pages for about and contact

--- components/DefaultComps/HeadBar.vue
+++ components/DefaultComps/HeadBar.vue
@@ -1,6 +1,18 @@
 <template>
   <v-app-bar collapse-on-scroll>
-    <h1>Imma Bar</h1>
+    <v-container>
+      <v-row>
+        <v-col class="col-3">
+          <h1 class="font-weight-light">Lindsey Renae</h1>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col class="col-3 offset-1">
+          <nuxt-link to="/about" class="links"><v-btn class="font-weight-light">About</v-btn></nuxt-link>
+
+          <nuxt-link to="/contact" class="links"><v-btn class="font-weight-light">Contact</v-btn></nuxt-link>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
 
@@ -11,5 +23,8 @@
 </script>
 
 <style  scoped>
-
+  .links {
+    text-decoration: none;
+    font-size: 32px;
+  }
 </style>
