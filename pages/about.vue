60d8a837d4a8ced8e97627ba5164886faec2884f
tboyd4 <tboyd1898@gmail.com>
2020-04-17 09:46:53 -0500

    added filler content with styles to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-        <h1>About</h1>
-    </div>
+  <div>
+    <div class="py-12"></div>
+    <div class="py-12"></div>
+    <v-container>
+      <v-row align-content="center">
+        <v-col class="col-md-12">
+          <hr />
+          <v-card flat class="my-4 mx-auto">
+            <v-card-text>
+              <h1 class="display-3 text--primary">Some Catchy way of introducing myself.</h1>
+              <br />
+              <p
+                class="display-1 text--primary"
+              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati fuga facere vel, rem in sunt perferendis totam sed? Quo sint nam explicabo illo, magni voluptatibus iure exercitationem? Minima, aut perferendis!</p>
+              <p
+                class="display-1 text--primary"
+              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Error non doloribus placeat modi fugiat amet accusantium voluptate obcaecati magni nostrum adipisci recusandae consequuntur ut tenetur blanditiis, expedita soluta molestias provident?</p>
+            </v-card-text>
+          </v-card>
+          <hr />
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="py-12"></div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
